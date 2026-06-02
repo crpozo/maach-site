a0e84bdf169ac17842f05fbbb1ec1879c074f253
Carlos Pozo <carlosricardo.pozo@hotmail.com>
2026-06-02 14:59:51 -0500

    home hero: add circuit-board trace texture overlay

--- src/pages/PageHome.tsx
+++ src/pages/PageHome.tsx
@@ -46,6 +46,12 @@ export default function PageHome() {
             zIndex: 1,
           }}
         />
+        {/* Circuit-board texture overlay */}
+        <div
+          className="tex-circuit"
+          aria-hidden
+          style={{ position: 'absolute', inset: 0, zIndex: 1, opacity: 0.12, pointerEvents: 'none' }}
+        />
         {/* Bold orange edge bar */}
         <div
           aria-hidden
